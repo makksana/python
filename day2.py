405788ddf3f1f7f76d7a8969865c396a7c16772e
Oksana Yershova <makksana@gmail.com>
2021-09-24 17:09:04 +0300

    Day 2 homework

---
+++ day2.py
@@ -0,0 +1,35 @@
+print('Day 2')
+print()
+friend_1_name = 'Mykhailo Hupalo'
+friend_1_email = 'mykhailo.hupalo@example.com'
+friend_1_age = 42
+friend_1_the_best = True
+
+friend_2_name = 'Maria Nepyivoda'
+friend_2_email = 'maria.nepyivoda@example.com'
+friend_2_age = 36
+friend_2_the_best = False
+
+friend_3_name = 'Tetiana Cheh'
+friend_3_email = 'tetiana.cheh@example.com'
+friend_3_age = 29
+friend_3_the_best = True
+
+friend_4_name = 'Bogdan Gutsulyak'
+friend_4_email = 'bohdan.gutsulyak@example.com'
+friend_4_age = 33
+friend_4_the_best = True
+
+friend_5_name = 'Ivan Petrov'
+friend_5_email = 'ivan.petrov@example.com'
+friend_5_age = 57
+friend_5_the_best = False
+
+print(f'{friend_4_name}, {friend_4_age} years old. Email: {friend_4_email}')
+print()
+
+my_name = 'Oksana'
+current_day = 'Wednesday'
+
+print(f'Good day {my_name}! {current_day} is a perfect day to learn some Python.')
+print('Good day %s! %s is a perfect day to learn some Python.' % (my_name, current_day))
