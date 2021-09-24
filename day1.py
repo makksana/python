0302d405a7a28aace1a9439859ea88806f820687
Oksana Yershova <makksana@gmail.com>
2021-09-24 17:06:42 +0300

    Day 1 homework

---
+++ day1.py
@@ -0,0 +1,7 @@
+print('Day 1')
+print()
+print('Hello World!', end='\n\n')
+line = '#'*5
+dots = '#\t#'.expandtabs(4)
+print(line, dots, dots, dots, line, sep='\n', end='\n\n')
+print(dots, dots, line, dots, dots, sep='\n')
